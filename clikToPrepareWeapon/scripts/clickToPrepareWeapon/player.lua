8126e012e3af80dc5ab505930aa81404f564b4fe
Drasenix <adowns@hotmail.fr>
2025-07-07 23:48:49 +0200

    Adding check on whether or not a window is opened before preparing weapon

--- clikToPrepareWeapon/scripts/clickToPrepareWeapon/player.lua
+++ clikToPrepareWeapon/scripts/clickToPrepareWeapon/player.lua
@@ -1,4 +1,5 @@
 local self = require('openmw.self')
+local core = require('openmw.core')
 local types = require('openmw.types')
 local ui = require('openmw.ui')
 
@@ -6,7 +7,9 @@ local selfObject = self
 
 local function onMouseButtonPress(button)
    local idleStanceActive = types.Actor.getStance(selfObject) == types.Player.STANCE.Nothing
-   if idleStanceActive then
+   local windowOpened = core.isWorldPaused()
+
+   if idleStanceActive and not windowOpened then
       if button == 1 then
          types.Actor.setStance(self, types.Player.STANCE.Weapon)
       end   
