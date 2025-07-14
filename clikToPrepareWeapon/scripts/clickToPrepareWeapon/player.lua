67935e2b94fe60cd3276329088d8646cd5d14c80
Drasenix <adowns@hotmail.fr>
2025-07-14 16:35:05 +0200

    Use modes instead of paused game to check for ability to prepare weapon

--- clikToPrepareWeapon/scripts/clickToPrepareWeapon/player.lua
+++ clikToPrepareWeapon/scripts/clickToPrepareWeapon/player.lua
@@ -1,18 +1,66 @@
 local self = require('openmw.self')
 local core = require('openmw.core')
+local UI = require('openmw.interfaces').UI
 local types = require('openmw.types')
-local ui = require('openmw.ui')
 local configPlayer = require('scripts.clickToPrepareWeapon.config.player')
 local l10n = core.l10n('clickToPrepareWeapon')
 
 local selfObject = self
 
+local function tableContains(table, value)
+   for i = 1, #table do
+      if table[i] == value then
+         return true
+      end
+   end
+   return false
+end
+
+local function preparingWeaponIsPossible()
+   local currentMode = UI.getMode()
+   
+   local modesTable = {
+      "ChargenClassGenerate",
+      "ChargenClass",
+      "Scroll",
+      "ChargenClassPick",
+      "Repair",
+      "ChargenClassReview",
+      "Loading",
+      "Journal",
+      "Rest",
+      "ChargenClassCreate",
+      "Book",
+      "ChargenRace",
+      "Container",
+      "Travel",
+      "LoadingWallpaper",
+      "Jail",
+      "Barter",
+      "SpellBuying",
+      "Alchemy",
+      "Dialogue",
+      "Companion",
+      "MainMenu",
+      "SpellCreation",
+      "Interface",
+      "Recharge",
+      "Enchanting",
+      "MerchantRepair",
+      "Training",
+      "ChargenName",
+      "LevelUp",
+      "ChargenBirth",
+      "QuickKeysMen"
+   }
+
+   return not tableContains(modesTable, currentMode)
+end
+
 local function onMouseButtonPress(button)
    local idleStanceActive = types.Actor.getStance(selfObject) == types.Player.STANCE.Nothing
-   local windowOpened = core.isWorldPaused()
    
-   
-   if idleStanceActive and not windowOpened then
+   if idleStanceActive and preparingWeaponIsPossible() then
       local clickNameForPreparingWeapon = l10n(configPlayer.options.s_Click)
       local clickIdEquivalent
       if clickNameForPreparingWeapon == "Left" then
