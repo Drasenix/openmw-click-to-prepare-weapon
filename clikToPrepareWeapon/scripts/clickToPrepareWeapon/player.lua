e2754fe6d70602c54bd3c2182e0e866641fbd1af
Drasenix <adowns@hotmail.fr>
2025-07-14 16:04:18 +0200

    Adding settings to choose click to use

--- clikToPrepareWeapon/scripts/clickToPrepareWeapon/player.lua
+++ clikToPrepareWeapon/scripts/clickToPrepareWeapon/player.lua
@@ -2,15 +2,29 @@ local self = require('openmw.self')
 local core = require('openmw.core')
 local types = require('openmw.types')
 local ui = require('openmw.ui')
+local configPlayer = require('scripts.clickToPrepareWeapon.config.player')
+local l10n = core.l10n('clickToPrepareWeapon')
 
 local selfObject = self
 
 local function onMouseButtonPress(button)
    local idleStanceActive = types.Actor.getStance(selfObject) == types.Player.STANCE.Nothing
    local windowOpened = core.isWorldPaused()
-
+   
+   
    if idleStanceActive and not windowOpened then
-      if button == 1 then
+      local clickNameForPreparingWeapon = l10n(configPlayer.options.s_Click)
+      local clickIdEquivalent
+      if clickNameForPreparingWeapon == "Left" then
+         clickIdEquivalent = 1
+      elseif clickNameForPreparingWeapon == "Middle" then
+         clickIdEquivalent = 2
+      elseif clickNameForPreparingWeapon == "Right" then
+         clickIdEquivalent = 3
+      else
+         return
+      end
+      if button == clickIdEquivalent then
          types.Actor.setStance(self, types.Player.STANCE.Weapon)
       end   
    end
---
+++ clikToPrepareWeapon/scripts/clickToPrepareWeapon/settings/player.lua
@@ -0,0 +1,38 @@
+local core = require('openmw.core')
+local I = require('openmw.interfaces')
+
+local l10n = core.l10n('ClickToPrepareWeapon')
+local versionString = "1.0.0"
+
+-- Settings page
+I.Settings.registerPage {
+    key = 'ClickToPrepareWeapon',
+    l10n = 'ClickToPrepareWeapon',
+    name = 'ConfigTitle',
+    description = l10n('ConfigSummary'):gsub('%%{version}', versionString),
+}
+
+I.Settings.registerGroup {
+    key = 'Settings/ClickToPrepareWeapon/ClientOptions',
+    page = 'ClickToPrepareWeapon',
+    l10n = 'ClickToPrepareWeapon',
+    name = 'ConfigCategoryClientOptions',
+    permanentStorage = true,
+    settings = {                
+        {
+            key = 's_Click',
+            renderer = 'select',
+            name = 'Click',
+            argument = {
+                l10n = 'ClickToPrepareWeapon',
+                items = {
+                    'Right',
+                    'Left',
+                    'Middle',
+                    'None'
+                }
+            },
+            default = 'Left',
+        }
+    },
+}
